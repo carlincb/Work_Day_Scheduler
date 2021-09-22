d256f7bdad0378208682daa469da264e175c26a9
carlincb <carlin.colleen@gmail.com>
2021-09-21 20:33:35 -0600

    Added initial files and created todo items in javascript file.

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// create a variable for the container div
+// create a function to make a singular time block
+    // create an html element each part of the time block
+    // add saving and display of local storage to each time block
+// write function to create a time block for each hour desired
+// append that timeblock to the container div
