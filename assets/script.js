fbe1018a6b9001fcb4f29942b41d515a0abf1805
carlincb <carlin.colleen@gmail.com>
2021-09-26 21:25:08 -0600

    Created initial function.

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,17 @@
     // add saving and display of local storage to each time block
 // write function to create a time block for each hour desired
 // append that timeblock to the container div
+function createTimeBlock(hour){
+    var container = $(".container");
+    var timeBlockDiv = $("<div>");
+    var hoursDiv = $("<div>");
+    var textArea = $("<textarea>");
+    var saveBtn = $("<button>");
+    var hourText = moment(hour.toString(), "h").format("hA");
+    timeBlockDiv.addClass("row time-block");
+    hoursDiv.addClass("hour col-md-1").append(hourText);
+    textArea.addClass("description col-md-10").attr("id", hour);
+    saveBtn.addClass("btn saveBtn col-md-1").append($("<i>").addClass("fas fa-save"));
+    timeBlockDiv.append(hoursDiv).append(textArea).append(saveBtn);
+    container.append(timeBlockDiv);
+}
